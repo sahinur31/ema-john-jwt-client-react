6b2b5bc484f68522deda26def97a1612f99fab94
sahinur31 <sahinur4841@gmail.com>
2021-11-02 12:22:56 +0600

    jwt

---
+++ src/components/Orders/Orders.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router';
+import useAuth from '../../hooks/useAuth';
+
+const Orders = () => {
+    const [orders, setOrders] = useState([]);
+    const { user } = useAuth();
+    const history = useHistory();
+    useEffect(() => {
+        fetch(`http://localhost:5000/orders?email=${user.email}`, {
+            headers: {
+                authorization: `Bearer ${localStorage.getItem('idToken')}`
+            }
+        })
+            .then(res => {
+                if (res.status === 200) {
+                    return res.json();
+                }
+                else if (res.status === 401) {
+                    history.push('/login');
+                }
+
+            })
+            .then(data => setOrders(data));
+    }, [])
+
+    return (
+        <div>
+            <h2>You have placed: {orders.length} Orders</h2>
+            <ul>
+                {orders.map(order => <li
+                    key={order._id}
+                >{order.name} : {order.email}</li>)}
+            </ul>
+        </div>
+    );
+};
+
+export default Orders;
